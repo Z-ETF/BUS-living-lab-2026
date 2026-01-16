6127a9278682a3082f2b4beda593765a50a58245
Your Name <your.email@example.com>
2026-01-16 21:45:43 +0100

    Rename main application class to BusLivingLabApplication

--- src/main/java/com/iot/soil/BusLivingLabApplication.java
+++ src/main/java/com/iot/soil/BusLivingLabApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SoilApplication {
+public class BusLivingLabApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SoilApplication.class, args);
-        System.out.println("Sensor Data API started successfully!");
+        SpringApplication.run(BusLivingLabApplication.class, args);
+        System.out.println("BUS Living Lab 2026 - Sensor Data API started successfully!");
     }
 }
+
